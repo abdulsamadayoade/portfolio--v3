41bdd1f021c9ce61d7293a8a82df2406559089e5
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2024-04-15 02:53:06 +0100

    update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,29 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./writing/**/*.mdx",
   ],
   theme: {
     extend: {
+      typography: {
+        quoteless: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+          },
+        },
+      },
       fontFamily: {
         mono: ["var(--font-geist-mono)", "serif"],
         sans: ["var(--font-commissioner-sans)", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 };
 export default config;
